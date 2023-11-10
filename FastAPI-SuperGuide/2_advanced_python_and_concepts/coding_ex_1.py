ca3c6dd01d3c15d4508ef0720a952439314a300a
rcmadden <dataengineer007@gmail.com>
2023-11-10 05:52:49 -0800

    initial attempt

--- FastAPI-SuperGuide/2_advanced_python_and_concepts/coding_ex_1.py
+++ FastAPI-SuperGuide/2_advanced_python_and_concepts/coding_ex_1.py
@@ -8,10 +8,45 @@ The content, which is a string
 An ID, which is an integer
 Likes, which is a list of strings
 
-The post should also have a field for comments, which is a list of comment models. The model should have:
+The post should also have a field for comments, 
+which is a list of comment models. 
+The model should have:
 An author, which is a string
 The comment, which is a string
 Likes, which is an integer
 
 Practice creating a social media post with whatever data you like, so long as it compiles.
 """
+from pydantic import BaseModel
+from typing import List, Optional
+
+class Comment(BaseModel):
+    author: str
+    comment: str
+    likes: int
+
+class Post(BaseModel):
+    author: str
+    co_author: Optional[str] = None
+    date: str
+    title: str
+    content: str
+    id: int
+    likes: List[str]
+    comments: List[Comment]
+
+new_post = Post(
+    author='Russia',
+    date='2021-01-01',
+    title='First Post',
+    content='hello world',
+    id=1,
+    likes=['Jenn', 'Sam'],
+    comments=[
+        Comment(author='Jenn', comment='Great post!', likes=3),
+        Comment(author='Sam', comment='Super post!', likes=6)
+        ]
+    )
+
+print(new_post)
+# author='Russia' co_author=None date='2021-01-01' title='First Post' content='hello world' id=1 likes=['Jenn', 'Sam'] comments=[Comment(author='Jenn', comment='Great post!', likes=3), Comment(author='Sam', comment='Super post!', likes=6)]
