546f028ebb19c2e1983e67ed3835261091eb8bb7
rcmadden <dataengineer007@gmail.com>
2023-11-10 19:21:53 -0800

    Updating car

--- FastAPI-SuperGuide/3_car_viewer_from_scratch/main.py
+++ FastAPI-SuperGuide/3_car_viewer_from_scratch/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Path, HTTPException, status, Body
 from pydantic import BaseModel, Field
 from typing import Optional, List, Dict
 # from datetime import date
@@ -34,3 +34,20 @@ def get_cars(number: Optional[str] = Query("10", max_length=3)):
         response.append(to_add)
     return response
 
+@app.get("/cars/{id}", response_model=Car)
+def get_car(id: int = Path(...,ge=0,lt=1000)):
+    car = cars.get(id)
+    if not car:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Car ID not found")
+    return car
+
+@app.post("/cars", status_code=status.HTTP_201_CREATED)
+def add_cars(body_cars: List[Car], min_id: Optional[int] = Body(0)):
+    if len(body_cars) < 1:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="No cars to add")
+    min_id = len(cars.values()) + min_id
+    for car in body_cars:
+        while cars.get(min_id):
+            min_id += 1
+        cars[min_id] = car
+        min_id += 1
