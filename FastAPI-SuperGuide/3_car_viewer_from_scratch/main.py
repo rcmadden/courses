5194b58eb0573620e6f64fecb2ef3c08a256c114
rcmadden <dataengineer007@gmail.com>
2023-11-11 14:03:59 -0800

    create main page, redirect and fix HTMLResponse

--- FastAPI-SuperGuide/3_car_viewer_from_scratch/main.py
+++ FastAPI-SuperGuide/3_car_viewer_from_scratch/main.py
@@ -1,18 +1,14 @@
 from fastapi import FastAPI, Query, Path, HTTPException, status, Body, Request
 from fastapi.encoders import jsonable_encoder
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel, Field
 from typing import Optional, List, Dict
-# from datetime import date
 from database import cars
 
 templates = Jinja2Templates(directory="templates")
 
-# get the current year from the date module
-# date = date.today()
-# current_year = date.year
-# print(current_year)
 
 class Car(BaseModel):
     make: Optional[str]
@@ -27,20 +23,18 @@ app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/")
+@app.get("/", response_class=RedirectResponse)
 def root(request: Request):
-    return templates.TemplateResponse("home.html",
-                                      { "request": request})
+    return RedirectResponse(url="/cars")
 
-
-@app.get("/cars", response_model=List[Dict[int, Car]])
-def get_cars(number: Optional[str] = Query("10", max_length=3)):
+@app.get("/cars", response_class=HTMLResponse)
+def get_cars(request: Request, number: Optional[str] = Query("10", max_length=3)):
     response = []
     for id, car in list(cars.items())[:int(number)]:
         to_add = {}
         to_add[id] = car
         response.append(to_add)
-    return response
+    return templates.TemplateResponse("index.html", {"request": request, "cars": response, "title": "Home"})
 
 @app.get("/cars/{id}", response_model=Car)
 def get_car(id: int = Path(...,ge=0,lt=1000)):
